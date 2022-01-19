393de7b71beefff4ca3ee67c9594887b9565621d
amin <amintavakkoli.fi@gmail.com>
2022-01-19 13:06:42 +0330

    create security package and write impl of loadUserByUsername

--- src/main/java/com/jwtexample/demo/DemoApplication.java
+++ src/main/java/com/jwtexample/demo/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -18,6 +19,11 @@ public class DemoApplication {
 
         SpringApplication.run(DemoApplication.class, args);
 
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
--- src/main/java/com/jwtexample/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/jwtexample/demo/service/impl/UserServiceImpl.java
@@ -7,9 +7,16 @@ import com.jwtexample.demo.repository.UserRepository;
 import com.jwtexample.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -17,35 +24,53 @@ import java.util.Set;
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
 
+    /** This method implemented from UserDetailedService
+    * and use for that , how to use from  user info in database */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user=getUser(username);
+        if (user==null){
+            log.error("User not found in database!");
+            throw new UsernameNotFoundException("User not found in database!");
+        }else{
+            log.info("User found in database: {}",username);
+        }
+        List<SimpleGrantedAuthority> authorities=new ArrayList<>();
+        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+
+        /**org.springframework.security.core.userdetails.User()*/
+        return new User(user.getUsername(),user.getPassword(),authorities);
+    }
+
+
     @Override
     public AppUser saveUser(AppUser user) {
-        log.info("user {} saved successfully",user.getName());
+        log.info("user {} saved successfully", user.getName());
         return userRepository.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
-        log.info("role {} saved successfully",role.getName());
+        log.info("role {} saved successfully", role.getName());
         return roleRepository.save(role);
     }
 
     @Override
     public void addRoleToAppUser(String username, String roleName) {
-        AppUser user=userRepository.findAppUserByUsername(username);
-        Role role=roleRepository.findByName(roleName);
+        AppUser user = userRepository.findAppUserByUsername(username);
+        Role role = roleRepository.findByName(roleName);
         user.getRoles().add(role);
-        log.info("role {} added to {} user",roleName,username);
-
+        log.info("role {} added to {} user", roleName, username);
     }
 
     @Override
     public AppUser getUser(String username) {
-        log.info("find {} user",username);
+        log.info("find {} user", username);
         return userRepository.findAppUserByUsername(username);
     }
 
@@ -54,4 +79,5 @@ public class UserServiceImpl implements UserService {
         log.info("get all user");
         return userRepository.findAll();
     }
+
 }
